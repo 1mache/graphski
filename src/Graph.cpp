bffd68e9923616c6316644efbaced7c4208413ac
1mache <miktor4925@gmail.com>
2025-04-07 00:20:21 +0300

    Started working on Graph class
    addNode and addEdge functions as well as necessary getters and setters. Laying the foundation

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,1 +1,41 @@
 #include "Graph.h"
+
+namespace graphski
+{
+	Graph::~Graph()
+	{
+		for(auto pair: m_adjList)
+		{
+			auto* nodePtr = pair.first;
+
+			for (auto* edge : pair.second)
+			{
+				delete edge;
+			}
+		}
+	}
+
+	uint8_t Graph::addNode(std::string name)
+	{
+		uint8_t id = nodeCount(); // TODO: this wont work if nodes can be deleted (ok for now)
+		// push back new node with empty edges list
+		m_adjList.push_back({ new Node(id, name), {} });
+
+		return id;
+	}
+
+	void Graph::addEdge(uint8_t fromNodeId, uint8_t toNodeId)
+	{
+		Node* fromPtr = m_adjList[fromNodeId].first,
+			* toPtr = m_adjList[toNodeId].first;
+
+		Edge* newEdge = new Edge(fromPtr, toPtr);
+
+		// increment degrees
+		fromPtr->setDOut(fromPtr->getDOut() + 1);
+		toPtr->setDIn(toPtr->getDIn() + 1);
+
+		// put new edge into the vector. TODO: sort by id toId inside vector
+		m_adjList[fromNodeId].second.push_back(newEdge);
+	}
+}
--- src/Graph.h
+++ src/Graph.h
@@ -9,7 +9,9 @@ namespace graphski
 	{
 		static constexpr uint8_t MAX_NODES  = UINT8_MAX;
 		static constexpr uint8_t INIT_NODES = 10;
-		using AdjacencyList = std::vector<std::pair<Node, std::vector<Edge>>>;
+		// adjacency list contains pairs node : its edges
+		// TODO: maybe have edges get ids of the nodes they need instead of pointers, then it could me on the stack
+		using AdjacencyList = std::vector<std::pair<Node*, std::vector<Edge*>>>;
 		
 		AdjacencyList m_adjList;
 
@@ -18,5 +20,21 @@ namespace graphski
 		{
 			m_adjList.reserve(INIT_NODES);
 		}
+
+		Graph(Graph&) = delete;
+		Graph& operator=(Graph&) = delete;
+		~Graph();
+
+		// how many nodes are there
+		uint8_t nodeCount() { return static_cast<uint8_t>(m_adjList.size()); }
+
+		// returns the copy of the adjacency list (TODO: find better alternative to "peek in")
+		AdjacencyList getAdjListCopy() { return m_adjList;}
+
+		// creates a node with empty edges list, returns its unique id
+		uint8_t addNode(std::string name = "");
+
+		// creates an edge between to given nodes, gets them by ids
+		void addEdge(uint8_t fromNodeId, uint8_t toNodeId);
 	};
 }
