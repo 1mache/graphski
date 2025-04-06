0b6459d8f29f34ef2aa1d99d9d0890d802224b71
1mache <miktor4925@gmail.com>
2025-04-06 17:42:58 +0300

    Node class in graphski namespace
    Added graphski namespace and Node class inside it with id, name and marked bool with getter/setter

---
+++ src/Node.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+
+namespace graphski 
+{
+	class Node
+	{
+		inline static uint8_t nodeCount = 0;
+		// node id
+		uint8_t m_id;
+		// if we want to give our node a name like A,B,C
+		std::string m_name;
+		// is the node marked, used for various algorithms
+		bool m_marked = false;
+
+	public:
+		explicit Node(std::string name = "") :m_id(nodeCount), m_name(name)
+		{
+			if (name.size() == 0) // if empty string was passed
+				m_name = std::to_string(m_id); //set the name to be the id (as string)
+			nodeCount++;
+		}
+
+		// marks the node, usually we want to mark true
+		bool mark(bool val = true)
+		{
+			m_marked = val;
+		}
+
+		bool isMarked() const
+		{
+			return m_marked;
+		}
+
+		uint8_t getId()
+		{
+			return m_id;
+		}
+
+		std::string getName()
+		{
+			return m_name;
+		}
+
+		void setName(std::string name)
+		{
+			m_name = name;
+		}
+	};
+}
