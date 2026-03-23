be6f95e86679aaa74a4977aad1de15d2456ea95a
1mache <miktor4925@gmail.com>
2026-03-24 01:09:38 +0200

    Removed DataNode and updated DataGraph to hold the data (Not tested yet)

--- include/DataGraph.h
+++ include/DataGraph.h
@@ -9,42 +9,32 @@ public:
     explicit DataGraph(size_t reserveCount = 0) : Graph(reserveCount) {}
     virtual ~DataGraph() = default;
 
-    // node const ref by id
-    const DataNode<T>& node(NodeId id) const override
+    NodeId addNode(std::string_view name = "")
     {
-        auto* dataNode = dynamic_cast<const DataNode<T>*>(Graph::node(id));
-        if (!dataNode)
-            throw std::runtime_error("Node not of type DataNode<T> found in DataGraph<T>");
-        return *dataNode;
+        throw std::runtime_error("addNode with name only is not implemented for DataGraph");
+    }
+    // have to give the graph data for this node
+    NodeId addNode(T data, std::string_view name = "")
+    {  
+        NodeId id = Graph::addNode(name);        
+        m_data.push_back(data);
+        return id;
     }
 
-    // shadows the base class version
-    NodeId addNode(T data, std::string name = "")
-    {  
-        if(nodeCount() >= MAX_NODES)
-            throw std::overflow_error("Maximum number of nodes exceeded");
-        // TODO: change when we can delete nodes
-        NodeId id = static_cast<NodeId>(nodeCount());
-        
-        Graph::insertNode(createNode(std::forward<T>(data), name));
-        return id;
+    const T& getNodeData(NodeId id) const
+    {
+        if (id >= m_data.size())
+            throw std::out_of_range("Node id out of range for data retrieval.");
+        return m_data[id];
+    }
+
+    T retrieveNodeData(NodeId id)
+    {
+        if (id >= m_data.size())
+            throw std::out_of_range("Node id out of range for data retrieval.");
+        return std::move(m_data[id]);
     }
     
 private:
-    // factory methods for creating nodes
-    std::unique_ptr<Node> createNode(T data, const std::string& name = "") const
-    {
-        return std::make_unique<DataNode<T>>(data, name);
-    }
-    
-    std::unique_ptr<Node> createNode(const Node* node) const
-    {
-        if(!node)
-            throw std::invalid_argument("Cannot create a node from a null pointer.");
-        auto* dataNode = dynamic_cast<const DataNode<T>*>(node);
-        if (!dataNode)
-            throw std::runtime_error("Node not of type DataNode<T> found in DataGraph<T>.");
-
-        return std::make_unique<DataNode<T>>(*dataNode);
-    }
+    std::vector<T> m_data;
 };
